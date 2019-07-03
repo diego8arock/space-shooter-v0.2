112baecf1970ad94c938004ad4b0194940749d2b
Diego Ochoa <diegoa2000alt@gmail.com>
2019-07-02 20:07:38 -0500

    Clean up code

--- core/player/Player.gd
+++ core/player/Player.gd
@@ -1,10 +1,12 @@
 extends KinematicBody2D
+class_name Player, "res://core/player/Player.tscn"
 
 var speed: float = 0.0
 var speed_rate: float = 1.0
-var velocity: Vector2 = Vector2(1,0)
+var direction: Vector2 = Vector2(1,0)
 var velocity_rotation: float = 0.0
 var velocity_rotation_speed: float = 3.0
+var velocity: Vector2
 
 const MAX_SPEED: float = 5.0
 const MAX_SPEED_REVERSE: float = -5.0
@@ -12,24 +14,20 @@ const MAX_SPEED_REVERSE: float = -5.0
 var is_forward: bool = true
 var is_reverse: bool = true
 
-var velocity_debug: Label
-var angle_debug: Label
-var speed_debug: Label
-var font: Font = preload("res://assets/fonts/12_kenny_high.tres")
+onready var debug = $SimpleDebug
+onready var ship_sprite = $Sprite
+onready var collision = $CollisionPolygon2D
 
-func _ready():
+onready var attack_zone = $AttackZone setget ,get_attack_zone_radius
 
-	velocity_debug = Label.new()
-	velocity_debug.add_font_override("font", font)
-	$Debug.add_child(velocity_debug)
+enum TIME_MODE {NORMAL, SLOW}
+var time_mode = TIME_MODE.NORMAL
 
-	angle_debug = Label.new()
-	angle_debug.add_font_override("font", font)
-	$Debug.add_child(angle_debug)
+func _ready():	
 
-	speed_debug = Label.new()
-	speed_debug.add_font_override("font", font)
-	$Debug.add_child(speed_debug)	
+	debug.add_label("velocity")
+	debug.add_label("angle")
+	debug.add_label("speed")
 
 func _process(delta: float) -> void:
 	
@@ -45,19 +43,26 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_left"):
 		turning_left(delta)
 		
-	if Input.is_mouse_button_pressed(BUTTON_RIGHT):
-		TimeManager.slow_mode()
-	else:
-		TimeManager.normal_mode()
-
 func _physics_process(delta: float) -> void:	
 	
-	var move: Vector2 = velocity.normalized().rotated(velocity_rotation) * TimeManager.adjust_speed(speed)
-	velocity_debug.text = str(move)
-	angle_debug.text = str(rad2deg(move.angle()))
-	speed_debug.text = str(speed)
-	$Sprite.global_rotation = velocity_rotation + deg2rad(90)
-	move_and_collide(move)
+	velocity = direction.normalized().rotated(velocity_rotation) * TimeManager.adjust_speed(speed)
+	debug.update_label("velocity", str(velocity))
+	debug.update_label("angle", str(rad2deg(velocity.angle())))
+	debug.update_label("speed", str(speed))
+	ship_sprite.global_rotation = velocity_rotation + deg2rad(90)
+	collision.global_rotation = velocity_rotation 
+	move_and_collide(velocity)
+	
+func _input(event: InputEvent) -> void:
+	
+	if Input.is_action_just_pressed("button_right"):
+		print("mouse")
+		if time_mode == TIME_MODE.NORMAL:
+			time_mode = TIME_MODE.SLOW
+			TimeManager.slow_mode()		
+		else:
+			time_mode = TIME_MODE.NORMAL
+			TimeManager.normal_mode()	
 	
 func foward(_delta: float) -> void:
 	
@@ -89,4 +94,7 @@ func turning_left(_delta: float) -> void:
 		
 func turning(_delta: float, _direction: int) -> void:
 	velocity_rotation += TimeManager.adjust_speed(velocity_rotation_speed) * _delta * _direction
+	
+func get_attack_zone_radius() -> float:
+	return (attack_zone.shape as CircleShape2D).radius
 	
